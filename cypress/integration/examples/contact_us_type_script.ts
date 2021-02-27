1cb804b5f3243f39ef199f8ecd8075870c2d01f0
Vakerin Eugene <utyfdfrthby08@gmail.com>
2021-02-27 12:09:36 +0200

    settings

---
+++ cypress/integration/examples/contact_us_type_script.ts
@@ -0,0 +1,11 @@
+describe("Test Contect Us form via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.google.com.ua");
+
+
+    });
+
+    it("Should NOT be able to submit a successful submission via contact us form as all fields are required", () => {
+
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+/// <reference types="../support" />
