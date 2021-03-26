cc4ec34629e1d236cdd41c5d7ad670194b2486af
evhenyivakeryn <utyfdfrthby08@gmail.com>
2021-03-27 00:35:49 +0200

    deleting python and selenium

--- selenium_javascript/testSelenium.js
+++
@@ -1,9 +0,0 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-(async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    try {
-        await driver.get('http://www.google.com/');
-    } finally {
-        await driver.quit();
-    }
-})();
