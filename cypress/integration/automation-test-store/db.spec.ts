4fac89e93308260a3cd371e320a93d994a34cf29
Eugene Vakerin <utyfdfrthby08@gmail.com>
2021-04-18 11:21:42 +0300

    db

--- cypress/integration/automation-test-store/db.spec.ts
+++ cypress/integration/automation-test-store/db.spec.ts
@@ -3,8 +3,22 @@ describe('DB', () => {
         // cy.sqlServer('select * from Persons').then(function (result) {
         //     console.log(result);
         // });
-        cy.sqlServer('select * from Persons where PersonID = 954').then((result) => {
-            console.log(result)
-        })
+        cy.sqlServer('select LastName from Persons where PersonID = 954').then((FirstName) => {
+            expect(FirstName).to.equal("Eugene");
+        });
+        cy.sqlServer("select FirstName from Persons where PersonID = 954").then((LastName) => {
+            expect(LastName).to.equal("Vakerin");
+        });
+
+        cy.sqlServer("select * from Persons where PersonID = 954").then((row) => {
+            expect(row).to.include(954);
+        });
+
+        cy.sqlServer("select * from Persons").then((rows) => {
+            console.log(rows);
+
+        });
+
+
     })
 })
